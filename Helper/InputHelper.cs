1e5e77f298be0fcc0ece11e4ccc82a079050c8a8
Nurbo322 <omuraliev.nurbolot@gmail.com>
2023-12-02 15:04:05 +0600

    Nurbo

---
+++ Helper/InputHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class InputHelper
+{
+    public static string ReadString(string inputMessage)
+    {
+        Console.WriteLine(inputMessage);
+        string? value = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            return value;
+        }
+
+        Console.WriteLine("Damn Son enter some letters ");
+        return ReadString(inputMessage);
+    }
+
+    public static int ReadNumber(string inputMessage)
+    {
+        string value = ReadString(inputMessage);
+        if (int.TryParse(value, out int number))
+        {
+            return number;
+        }
+
+        Console.WriteLine("Digits Mason!!!");
+        return ReadNumber(inputMessage);
+    }
+}
